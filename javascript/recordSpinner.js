0e47e9802fbbd35db6c634f28d2cf71681a08890
Brady Pacheco <bpacheco@students.ccu.edu>
2024-04-30 14:30:54 -0600

    Added margin to home page. Deleted unecessary JS

--- javascript/recordSpinner.js
+++ javascript/recordSpinner.js
@@ -1,12 +1,3 @@
-/* function spinRecord (record) {
-    document.getElementById(record).translate(0, 100);
-} */
-
-
-    /* let height = document.getElementById(record).style.height
-    document.getElementById(record).style.height = height - 250; */
-
-
     function spinRecord(recordId) {
         var recordSpin = document.getElementById(recordId);
         if (recordSpin) {
